b60199e47f9003bca681fed6553b4ccc9457aafe
anuj <openmindedlearning@gmail.com>
2023-03-13 15:04:11 +0530

    rn-hooks-course-02-working-with-content - home and components screens.

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +15,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Components" component={ComponentsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
